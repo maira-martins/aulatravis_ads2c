cb01be57ec6d7c8c9be72ee9349ba7b62c19c646
maira-martins <mah-martins@live.com>
2020-04-09 21:21:15 -0300

    travis

--- src/jogovelha.py
+++ src/jogovelha.py
@@ -4,7 +4,7 @@ def inicializar():
     for i in range(3):
         linha = [ ]
         for j in range(3):
-            linha.ap pend(".")
+            linha.append(".")
         tab.append(linha)
     return tab
 def main( ):
